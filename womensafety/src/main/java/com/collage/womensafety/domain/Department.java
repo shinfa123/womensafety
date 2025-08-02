9bf0bbe574a507e1360deff581f9f72f8bf90341
shinfa <24mcalo4@meaec.edu.in>
2025-08-03 00:53:19 +0530

    changes

--- womensafety/src/main/java/com/collage/womensafety/domain/Department.java
+++ womensafety/src/main/java/com/collage/womensafety/domain/Department.java
@@ -1,6 +1,15 @@
 package com.collage.womensafety.domain;
 
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
 public class Department {
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
 	public int departmentId;
 	public String departmentName;
 
